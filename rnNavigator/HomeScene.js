d7c83790a0e38ec40030de396040f86f7735c09c
gergo.bogdan@accedo.tv <greg@grelution.com>
2017-01-28 23:45:30 +0100

    changed styling and added PropType definitions

--- rnNavigator/HomeScene.js
+++ rnNavigator/HomeScene.js
@@ -4,8 +4,14 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { View, Text, Navigator, StyleSheet } from 'react-native';
+import React, { Component, PropTypes } from 'react';
+import {
+  View,
+  Text,
+  Navigator,
+  StyleSheet,
+  TouchableHighlight
+} from 'react-native';
 
 export default class HomeScene extends Component {
   static get defaultProps() {
@@ -17,7 +23,17 @@ export default class HomeScene extends Component {
   render() {
     return (
       <View style={styles.homeScene}>
-        <Text style={styles.title}>This is {this.props.title}</Text>
+        <Text style={styles.title}>{this.props.title}</Text>
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.props.goToNextScene}>
+            <Text style={styles.buttonText}>Go Deeper</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.props.goToPreviousScene}>
+            <Text style={styles.buttonText}>Back</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -27,11 +43,30 @@ const styles = StyleSheet.create({
   homeScene: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: 'red'
+    alignItems: 'center',
+    backgroundColor: '#09573F'
   },
   title: {
     fontSize: 30,
     textAlign: 'center',
     color: 'white'
+  },
+  button: {
+    width: 150,
+    height: 50,
+    backgroundColor: '#4D9B7F',
+    marginTop: 10,
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 20,
+    textAlign: 'center'
   }
 });
+
+HomeScene.propTypes = {
+  title: PropTypes.string.isRequired,
+  goToNextScene: PropTypes.func.isRequired,
+  goToPreviousScene: PropTypes.func.isRequired,
+};
